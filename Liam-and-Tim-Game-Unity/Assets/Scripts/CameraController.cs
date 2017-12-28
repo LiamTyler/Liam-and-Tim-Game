694194d1eb24cb4481d5e9d3b523268f898b47d5
Tim Nelson <timnelson94@gmail.com>
2017-12-28 15:58:59 -0600

    camera controller

---
+++ Liam-and-Tim-Game-Unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		float x = player.transform.position.x;
+		float y = player.transform.position.y;
+		transform.position = new Vector3 (x, y, -10.0f);
+		offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
+
+
