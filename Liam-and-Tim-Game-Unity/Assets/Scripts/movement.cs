45d11b2ac0d7d4af7843a8f91adb4070fd35896a
Tim Nelson <timnelson94@gmail.com>
2017-12-28 17:09:53 -0600

    move speed and script change to fixed update

--- Liam-and-Tim-Game-Unity/Assets/Scripts/movement.cs
+++ Liam-and-Tim-Game-Unity/Assets/Scripts/movement.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class movement : MonoBehaviour {
-
+	public float moveSpeed = 0.5f;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        transform.position += move * 2.0F * Time.deltaTime;
+        transform.position += move * moveSpeed;
 	}
 }
