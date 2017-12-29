3e5df6974b558f4b3fa5a63a0be63d8e4d3cd189
Tim Nelson <timnelson94@gmail.com>
2017-12-29 11:35:51 -0600

    movement script small changes

--- Liam-and-Tim-Game-Unity/Assets/Scripts/movement.cs
+++ Liam-and-Tim-Game-Unity/Assets/Scripts/movement.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class movement : MonoBehaviour {
     public GameObject Bullet;
-	public float moveSpeed = 0.5f;
+	public float moveSpeed;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += move * moveSpeed * Time.deltaTime;
 
