34607c7705b5b92758b1fe8fff870084aeb0bc86
Liam Tyler <tyler.liam7@gmail.com>
2017-12-29 05:14:25 -0800

    Shooting bullet, moving character, directional light

--- Liam-and-Tim-Game-Unity/Assets/Scripts/movement.cs
+++ Liam-and-Tim-Game-Unity/Assets/Scripts/movement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class movement : MonoBehaviour {
+    public GameObject Bullet;
 	public float moveSpeed = 0.5f;
 	// Use this for initialization
 	void Start () {
@@ -10,8 +11,21 @@ public class movement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        transform.position += move * moveSpeed;
+        transform.position += move * moveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyUp("space"))
+        {
+            //  GameObject newBullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+            GameObject bullet = Instantiate(
+                Bullet,
+                transform.position,
+                Quaternion.identity);
+
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.right * 4;
+
+            Destroy(bullet, 2.0f);
+        }
 	}
 }
