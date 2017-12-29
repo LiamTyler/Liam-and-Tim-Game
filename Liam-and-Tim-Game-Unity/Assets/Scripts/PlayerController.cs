d086879eac20c4b0c0d2d1c4515af8b68ba696a5
Tim Nelson <timnelson94@gmail.com>
2017-12-29 11:56:29 -0600

    change move script to PlayerCtrlr

---
+++ Liam-and-Tim-Game-Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject Bullet;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// handle input
+	void Update() {
+		if (Input.GetKeyUp("space"))
+		{
+			//  GameObject newBullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+			GameObject bullet = Instantiate(
+				Bullet,
+				transform.position,
+				Quaternion.identity);
+
+			bullet.GetComponent<Rigidbody>().velocity = bullet.transform.right * 4;
+
+			Destroy(bullet, 2.0f);
+		}
+	}
+	
+	void FixedUpdate () {
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * moveSpeed;
+	}
+}
