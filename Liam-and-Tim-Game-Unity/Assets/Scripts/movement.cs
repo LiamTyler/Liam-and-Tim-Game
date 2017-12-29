b749192c36d137f233fa875ea2b3f8d95ea98983
Liam Tyler <tyler.liam7@gmail.com>
2017-12-28 22:24:47 -0800

    plz work

---
+++ Liam-and-Tim-Game-Unity/Assets/Scripts/movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * 2.0F * Time.deltaTime;
+	}
+}
