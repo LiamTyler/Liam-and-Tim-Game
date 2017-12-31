6aa3bffff01e1f596a51cd155076e452bc25e178
Liam Tyler <tyler.liam7@gmail.com>
2017-12-31 09:09:34 -0800

    fix(collision): Player doesnt move into the gun anymore

--- Liam-and-Tim-Game-Unity/Assets/Scripts/PlayerController.cs
+++ Liam-and-Tim-Game-Unity/Assets/Scripts/PlayerController.cs
@@ -2,28 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : PlayerParent {
+public class PlayerController : MonoBehaviour
+{
+    public float m_MoveSpeed;
+    public GameObject m_Bullet;
 
-	public GameObject Bullet;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    }
 
-	// handle input
-	void Update() {
-		if (Input.GetKeyUp("space"))
-		{
-			//  GameObject newBullet = Instantiate(Bullet, transform.position, Quaternion.identity);
-			GameObject bullet = Instantiate(
-				Bullet,
-				transform.position,
-				Quaternion.identity);
+    // handle input
+    void Update()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            //  GameObject newBullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+            GameObject bullet = Instantiate(
+                m_Bullet,
+                transform.position,
+                Quaternion.identity);
 
-			bullet.GetComponent<Rigidbody>().velocity = bullet.transform.right * 4;
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.right * 4;
 
-			Destroy(bullet, 2.0f);
-		}
-	}	
+            Destroy(bullet, 2.0f);
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        // transform.position += move * m_MoveSpeed;
+        this.GetComponent<Rigidbody2D>().velocity = move * m_MoveSpeed;
+    }
 }
